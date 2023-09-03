1e8e437ec247a92e884aeee613e03beacb4c5ddb
Jordan Ayotte <jordanayotte13@outlook.com>
2023-09-03 01:37:23 -0400

    styled cat of the year component to look more like the style of the reviews on homepage, changed about section typo and made text in section start left

--- src/pages/Cats.js
+++ src/pages/Cats.js
@@ -3,15 +3,24 @@ import React from 'react'
 function Cats() {
   return (
     <div className='bg-neutral-900 text-neutral-300'>
-      <section className='flex flex-col items-center'>
-      <h2 className='text-center text-3xl'>Cathub</h2>
-      <article className='bg-yellow-700 w-fit rounded-lg flex flex-col items-center text-xl p-4'>
-        <h3>Featured Cat:</h3>
-        <p>Oscar</p>
-        <img alt='oscar the cat' className='rounded-full w-20 h-20 bg-red-500'></img>
-        <p>Favorite food: Kraft Singles</p>
-        <p>Superpower: Super strength and speed at 2AM.</p>
-        <p>Origin Story: lorem blah blah blag</p>
+      <section className='flex flex-col items-center first-letter gap-4'>
+      <strong className='text-center text-7xl'>Cathub</strong>
+      <article className='bg-gradient-to-r from-pink-900 to-neutral-800 w-fit rounded-lg flex flex-col items-center gap-2 text-xl p-4'>
+        <strong className='text-2xl'>Cat of the year</strong>
+        <h3>Oscar</h3>
+        <img alt='oscar the cat' src='https://www.onegreenplanet.org/wp-content/uploads/2014/07/m1lead.jpg' className='rounded-full h-12 w-12 md:h-24 md:w-24 object-cover'></img>
+        <div className='text-center'>
+        <h4 className='text-sm text-opacity-50'>Favorite food</h4>
+        <p>Kraft Singles</p>
+        </div>
+        <div className='text-center'>
+        <h4 className='text-sm text-opacity-50'>Superpower</h4>
+        <p>Super strength and speed at 2AM</p>
+        </div>
+        <div className='text-center'>
+        <h4 className='text-sm text-opacity-50'>Origin Story</h4>
+        <p>lorem blah blah blag</p>
+        </div>
       </article>
       </section>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,12 @@ function Home() {
   return (
     <div className='bg-neutral-900 text-neutral-300'>
       <div className='w-full parallax' alt='cats in space'></div>
-      <section className='px-2 text-xl text-center md:text-3xl'>
+      <section className='px-2 text-xl md:text-3xl'>
         <h1 className='text-6xl md:text-9xl text-center py-4'><strong>Coffee Cat</strong></h1>
         <p>Here at Coffee Cat our mission is simple:
         <br></br><strong>Take over all of humanity and replace humans with AI-enhanced cat cyborgs.</strong></p>
         <p>In all seriousness, our team of experts have searched the globe for the best coffees and teas and put them all right here for your convenience.</p>
-        <br></br>
-        <p>Don't believe us? Well then take a look at some of our non-paid-off customer reviews and you might just think twice.</p>
+        <p>Don't believe us? Well take a look at some of our non-paid-off customer reviews and you might just think twice.</p>
       </section>
       <h2 className='text-4xl md:text-7xl text-center py-4 customer-reviews'>Customer Reviews</h2>
       <ul className='px-2 w-full flex flex-col md:flex-row gap-2 text-xl md:text-center md:text-3xl md:justify-evenly'>
