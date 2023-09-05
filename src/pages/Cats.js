4b3cef06823e0ab05874c0656ee13516a37d59fb
Jordan Ayotte <jordanayotte13@outlook.com>
2023-09-05 00:38:06 -0400

    populated honroable mentions with actual cards basicly styled

--- src/pages/Cats.js
+++ src/pages/Cats.js
@@ -19,19 +19,109 @@ function Cats() {
         </div>
         <div className='text-center'>
         <h4 className='text-sm md:text-lg text-opacity-50'>Origin Story</h4>
-        <p>lorem blah blah blag</p>
+        <p>lorem blah blah blah...</p><button className='bg-neutral-800 rounded-md px-4 py-2'>See more</button>
         </div>
       </article>
       </section>
       <section className='flex flex-wrap w-full justify-center gap-2'>
         <h2 className='text-2xl md:text-7xl underline w-full text-center'>Honorable Mentions</h2>
-        <ul className='flex flex-wrap w-full'>
-          <li className='w-full md:w-1/3 bg-red-500'>Cat 1</li>
-          <li className='w-full md:w-1/3 bg-red-500'>Cat 1</li>
-          <li className='w-full md:w-1/3 bg-red-500'>Cat 1</li>
-          <li className='w-full md:w-1/3 bg-red-500'>Cat 1</li>
-          <li className='w-full md:w-1/3 bg-red-500'>Cat 1</li>
-          <li className='w-full md:w-1/3 bg-red-500'>Cat 1</li>
+        <ul className='grid sm:grid-cols-2 md:grid-cols-3 grid-rows-2 place-items-center gap-2 w-full'>
+          <li className='bg-gradient-to-r from-pink-900 to-neutral-800 w-full rounded-lg flex flex-col items-center gap-2 p-4'>
+            <h3>Name</h3>
+            <img alt='oscar the cat' src='https://www.onegreenplanet.org/wp-content/uploads/2014/07/m1lead.jpg' className='rounded-full h-12 w-12 md:h-24 md:w-24 object-cover'></img>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Favorite food</h4>
+            <p>Kraft Singles</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Superpower</h4>
+            <p>Super strength and speed at 2AM</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Origin Story</h4>
+            <p>lorem blah blah blah...</p><button className='bg-neutral-800 rounded-md px-4 py-2'>See more</button>
+            </div>
+          </li>
+          <li className='bg-gradient-to-r from-pink-900 to-neutral-800 w-full rounded-lg flex flex-col items-center gap-2 p-4'>
+            <h3>Name</h3>
+            <img alt='oscar the cat' src='https://www.onegreenplanet.org/wp-content/uploads/2014/07/m1lead.jpg' className='rounded-full h-12 w-12 md:h-24 md:w-24 object-cover'></img>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Favorite food</h4>
+            <p>Kraft Singles</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Superpower</h4>
+            <p>Super strength and speed at 2AM</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Origin Story</h4>
+            <p>lorem blah blah blah...</p><button className='bg-neutral-800 rounded-md px-4 py-2'>See more</button>
+            </div>
+          </li>
+          <li className='bg-gradient-to-r from-pink-900 to-neutral-800 w-full rounded-lg flex flex-col items-center gap-2 p-4'>
+            <h3>Name</h3>
+            <img alt='oscar the cat' src='https://www.onegreenplanet.org/wp-content/uploads/2014/07/m1lead.jpg' className='rounded-full h-12 w-12 md:h-24 md:w-24 object-cover'></img>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Favorite food</h4>
+            <p>Kraft Singles</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Superpower</h4>
+            <p>Super strength and speed at 2AM</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Origin Story</h4>
+            <p>lorem blah blah blah...</p><button className='bg-neutral-800 rounded-md px-4 py-2'>See more</button>
+            </div>
+          </li>
+          <li className='bg-gradient-to-r from-pink-900 to-neutral-800 w-full rounded-lg flex flex-col items-center gap-2 p-4'>
+            <h3>Name</h3>
+            <img alt='oscar the cat' src='https://www.onegreenplanet.org/wp-content/uploads/2014/07/m1lead.jpg' className='rounded-full h-12 w-12 md:h-24 md:w-24 object-cover'></img>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Favorite food</h4>
+            <p>Kraft Singles</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Superpower</h4>
+            <p>Super strength and speed at 2AM</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Origin Story</h4>
+            <p>lorem blah blah blah...</p><button className='bg-neutral-800 rounded-md px-4 py-2'>See more</button>
+            </div>
+          </li>
+          <li className='bg-gradient-to-r from-pink-900 to-neutral-800 w-full rounded-lg flex flex-col items-center gap-2 p-4'>
+            <h3>Name</h3>
+            <img alt='oscar the cat' src='https://www.onegreenplanet.org/wp-content/uploads/2014/07/m1lead.jpg' className='rounded-full h-12 w-12 md:h-24 md:w-24 object-cover'></img>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Favorite food</h4>
+            <p>Kraft Singles</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Superpower</h4>
+            <p>Super strength and speed at 2AM</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Origin Story</h4>
+            <p>lorem blah blah blah...</p><button className='bg-neutral-800 rounded-md px-4 py-2'>See more</button>
+            </div>
+          </li>
+          <li className='bg-gradient-to-r from-pink-900 to-neutral-800 w-full rounded-lg flex flex-col items-center gap-2 p-4'>
+            <h3>Name</h3>
+            <img alt='oscar the cat' src='https://www.onegreenplanet.org/wp-content/uploads/2014/07/m1lead.jpg' className='rounded-full h-12 w-12 md:h-24 md:w-24 object-cover'></img>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Favorite food</h4>
+            <p>Kraft Singles</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Superpower</h4>
+            <p>Super strength and speed at 2AM</p>
+            </div>
+            <div className='text-center'>
+            <h4 className='text-sm md:text-lg text-opacity-50'>Origin Story</h4>
+            <p>lorem blah blah blah...</p><button className='bg-neutral-800 rounded-md px-4 py-2'>See more</button>
+            </div>
+          </li>
         </ul>
       </section>
     </div>
