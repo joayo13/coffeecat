2b0697eef76aed76620fef83bd0df3f79f0844ca
Jordan Ayotte <jordanayotte13@outlook.com>
2023-09-13 14:20:05 -0400

    fixed totalprice to be fixed to 2 decimal point, added overflow y scroll to shopping cart and changed scrollbar style for whoel app

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -11,8 +11,7 @@ function ShoppingCart({setShoppingCartActive, slide, cartUpdated}) {
   }
   const cartData = JSON.parse(localStorage.getItem('cart'))
   setCart(cartData)
-  cartData.length === 1 ? setTotalPrice(cartData[0].price * cartData[0].quantity) :
-  setTotalPrice(cartData.reduce((total, curVal) => total + (curVal.price * curVal.quantity), 0))
+  setTotalPrice(cartData.reduce((total, curVal) => total + (curVal.price * curVal.quantity), 0).toFixed(2))
   },[cartUpdated])
   return (
     <div style={slide === 'left' ? {animation: 'slideLeft 1s ease forwards'} : {animation: 'slideRight 1s ease forwards'}} className='w-full text-center p-2 md:w-2/5 z-20 fixed right-0 top-0 h-full bg-neutral-900'>
@@ -22,7 +21,7 @@ function ShoppingCart({setShoppingCartActive, slide, cartUpdated}) {
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
         </svg>
       </button>
-      <ul className='flex flex-col gap-2'>{cart.map((item) => <CartItem cartItemData={item}/>)}</ul>
+      <ul className='flex flex-col gap-2 h-[90%] overflow-y-scroll'>{cart.map((item) => <CartItem cartItemData={item}/>)}</ul>
       <div className='absolute bottom-2 w-full flex justify-center'>
       <button className='bg-neutral-300 text-neutral-800 font-extrabold py-2 px-4 rounded-full'>Checkout ({totalPrice})</button>
       <button onClick={() => clearCart() }>Clear</button>
