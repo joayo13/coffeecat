db673e6a423484226d22715d93ae35b95520ad08
Jordan Ayotte <jordanayotte13@outlook.com>
2023-09-20 14:14:33 -0400

    fully populated cats page

--- src/pages/Cats.js
+++ src/pages/Cats.js
@@ -22,6 +22,36 @@ function Cats() {
           <p>Smelling up the house at 1am</p>
         </span>
         </section>
+        <section className='flex flex-col w-full items-center py-2 px-2'>
+        <strong className='text-3xl text-center pb-2 md:text-6xl max-w-lg'>Loudest Meow- Turk</strong>
+        <img className='md:max-w-lg py-2' alt='poo' src='https://www.hillspet.com/content/dam/cp-sites/hills/hills-pet/en_us/exported/cat-care/images/turkish-van-cat-lying-on-bed.jpg'></img>
+        <p className='max-w-lg py-2'>Incididunt Lorem laboris minim excepteur eiusmod mollit qui elit incididunt ad exercitation qui labore.
+         Qui qui irure amet incididunt. Sit commodo consectetur aute esse.
+          Nulla aliquip enim non tempor non nisi velit deserunt. Ea irure sint laborum deserunt est ad culpa. Adipisicing in eu officia quis ullamco.</p>
+        <span className='w-full max-w-lg flex gap-2'>
+          <strong>Favorite Food:</strong>
+          <p>Beans</p>
+        </span>
+        <span className='w-full max-w-lg flex gap-2'>
+          <strong>Superpower:</strong>
+          <p>Licking all the salt off your fingers in 0.3 seconds</p>
+        </span>
+        </section>
+        <section className='flex flex-col w-full items-center py-2 px-2'>
+        <strong className='text-3xl text-center pb-2 md:text-6xl max-w-lg'>Highest Jump- Benji</strong>
+        <img className='md:max-w-lg py-2' alt='poo' src='https://www.thesprucepets.com/thmb/2k2htmw3td3UE3CM9BNx7Yl5Rdo=/2750x0/filters:no_upscale():strip_icc()/are-calico-cats-always-female-3384568-hero-5103f8c4cf584988905599bdba0cab72.jpg'></img>
+        <p className='max-w-lg py-2'>Incididunt Lorem laboris minim excepteur eiusmod mollit qui elit incididunt ad exercitation qui labore.
+         Qui qui irure amet incididunt. Sit commodo consectetur aute esse.
+          Nulla aliquip enim non tempor non nisi velit deserunt. Ea irure sint laborum deserunt est ad culpa. Adipisicing in eu officia quis ullamco.</p>
+        <span className='w-full max-w-lg flex gap-2'>
+          <strong>Favorite Food:</strong>
+          <p>Tostitos</p>
+        </span>
+        <span className='w-full max-w-lg flex gap-2'>
+          <strong>Superpower:</strong>
+          <p>Extremely high catnip tolerance</p>
+        </span>
+        </section>
     </div>
   )
 }
