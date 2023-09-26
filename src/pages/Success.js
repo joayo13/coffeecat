3bdd281c71246c28b72ac32d28ed23ccf769f5f2
Jordan Ayotte <jordanayotte13@outlook.com>
2023-09-26 14:33:30 -0400

    updated

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Success() {
   return (
     <div className='text-neutral-300 text-center h-screen'>
-      <strong className='text-3xl md:text-6xl'>Transaction Successful.</strong>
+      <strong className='text-3xl md:text-6xl'>Transaction Successful. Thanks for supporting this test project.</strong>
       <Link className='underline block py-4' to='/'>Return to Home</Link>
     </div>
   )
