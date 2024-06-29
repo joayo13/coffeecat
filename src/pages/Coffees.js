fd418d9fcdac4a53b92e88b6fe48532b1e544bd7
Jordan Ayotte <jordanayotte13@outlook.com>
2024-06-29 14:50:20 -0400

    added skeleton load to coffees list page

--- src/pages/Coffees.js
+++ src/pages/Coffees.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { scrollToTop } from '../helpers/scrollToTop'
 import coffeeItems from '../products/coffeeItems'
@@ -7,66 +7,37 @@ function Coffees() {
   useEffect(() => {
     scrollToTop()
   },[])
+  const [loading, setLoading] = useState(Array(coffeeItems.length).fill(true));
+
+  const handleImageLoad = (index) => {
+    setLoading(prevLoading => {
+      const newLoading = [...prevLoading];
+      newLoading[index] = false;
+      return newLoading;
+    });
+  };
   return (
     <div className='bg-neutral-900 text-neutral-300 pt-16'>
       <section className='text-center flex flex-col items-center'>
       <strong className='text-6xl py-2'>Cat Café</strong>
       <h3>All Products</h3>
       <ul className='flex flex-col md:flex-row md:max-w-screen-lg justify-center gap-2 flex-wrap'>
-        <Link to='0'>
-        <li className='w-72 h-96 flex flex-col justify-evenly p-4'>
-          <img className='h-3/4 w-full object-cover' alt={coffeeItems[0].title} src={coffeeItems[0].image}></img>
-          <strong>{coffeeItems[0].title}</strong>
-        </li>
-        </Link>
-        <Link to='1'>
-        <li className='w-72 h-96 flex flex-col justify-evenly p-4'>
-          <img className='h-3/4 w-full' alt={coffeeItems[1].title} src={coffeeItems[1].image}></img>
-          <strong>{coffeeItems[1].title}</strong>
-        </li>
-        </Link>
-        <Link to='2'>
-        <li className='w-72 h-96 flex flex-col justify-evenly p-4'>
-          <img className='h-3/4 w-full' alt={coffeeItems[2].title} src={coffeeItems[2].image}></img>
-          <strong>{coffeeItems[2].title}</strong>
-        </li>
-        </Link>
-        <Link to='3'>
-        <li className='w-72 h-96 flex flex-col justify-evenly p-4'>
-          <img className='h-3/4 w-full' alt={coffeeItems[3].title} src={coffeeItems[3].image}></img>
-          <strong>{coffeeItems[3].title}</strong>
-        </li>
-        </Link>
-        <Link to='4'>
-        <li className='w-72 h-96 flex flex-col justify-evenly p-4'>
-          <img className='h-3/4 w-full' alt={coffeeItems[4].title} src={coffeeItems[4].image}></img>
-          <strong>{coffeeItems[4].title}</strong>
-        </li>
-        </Link>
-        <Link to='5'>
-        <li className='w-72 h-96 flex flex-col justify-evenly p-4'>
-          <img className='h-3/4 w-full' alt={coffeeItems[5].title} src={coffeeItems[5].image}></img>
-          <strong>{coffeeItems[5].title}</strong>
-        </li>
-        </Link>
-        <Link to='6'>
-        <li className='w-72 h-96 flex flex-col justify-evenly p-4'>
-          <img className='h-3/4 w-full' alt={coffeeItems[6].title} src={coffeeItems[6].image}></img>
-          <strong>{coffeeItems[6].title}</strong>
-        </li>
-        </Link>
-        <Link to='7'>
-        <li className='w-72 h-96 flex flex-col justify-evenly p-4'>
-          <img className='h-3/4 w-full' alt={coffeeItems[7].title} src={coffeeItems[7].image}></img>
-          <strong>{coffeeItems[7].title}</strong>
-        </li>
-        </Link>
-        <Link to='8'>
-        <li className='w-72 h-96 flex flex-col justify-evenly p-4'>
-          <img className='h-3/4 w-full' alt={coffeeItems[8].title} src={coffeeItems[8].image}></img>
-          <strong>{coffeeItems[8].title}</strong>
-        </li>
-        </Link>
+      {coffeeItems.map((item, index) => (
+            <Link to={`${index}`} key={index}>
+              <li className='w-72 h-96 flex flex-col justify-evenly p-4'>
+                {loading[index] && (
+                  <div className="h-3/4 w-full bg-neutral-800 rounded-lg animate-pulse"></div>
+                )}
+                <img
+                  className={`h-3/4 w-full object-cover ${loading[index] ? 'hidden' : ''}`}
+                  alt={item.title}
+                  src={item.image}
+                  onLoad={() => handleImageLoad(index)}
+                />
+                <strong>{item.title}</strong>
+              </li>
+            </Link>
+          ))}
       </ul>
       <p>Page 1 of 1</p>
       </section>
--- src/products/coffeeItems.js
+++ src/products/coffeeItems.js
@@ -8,87 +8,88 @@ import taraTurned from '../images/TaraTurned.webp'
 import albertTurned from '../images/AlbertTurned.webp'
 import mikaTurned from '../images/MikaTurned.webp'
 
-const coffeeItems = {
-    0: {
-      title: `Kitty Town Coffee- Robust Roy Chocolate`,
-      price: 10.99,
-      id: 1,
-      quantity: 1,
-      image: robustRoy,
-      category: 'medium roast',
-      desc: `Indulge your senses in a delightful feline adventure with our newest coffee creation - Robust Roy Chocolate. Crafted with the utmost care and dedication, this extraordinary coffee blend combines the bold strength of a robust roast with the sweet and velvety notes of decadent chocolate. It's the ideal choice for those who crave a luxurious coffee experience that's as bold as a lion yet as smooth as a cat's stride.`
-    },
-    1: {
-      title: `Kitty Town Coffee- Simba's Sumatra`,
-      price: 10.99,
-      id: 1,
-      quantity: 1,
-      image: simbaTurned,
-      category: 'medium roast',
-      desc: `Prepare to embark on a majestic coffee adventure with Simba's Sumatra, a truly regal offering from Kitty Town Coffee. Just like the great lion king Simba himself, this coffee blend reigns supreme, boasting bold character, deep flavor, and a rich heritage that transports you to the exotic landscapes of Sumatra with every sip.`
-    },  
-    2: {
-      title: `Kitty Town Coffee- Delilah's Decaf Coffee`,
-      price: 10.99,
-      id: 1,
-      quantity: 1,
-      image: delilahTurned,
-      category: 'decaf',
-      desc: `Experience the serenity of a calm, caffeine-free cup of coffee with Delilah's Decaf, the latest enchanting offering from Kitty Town Coffee. This soothing decaffeinated blend allows you to unwind and indulge in the rich, comforting flavors of coffee without the buzz, making it perfect for anytime relaxation.`
-    },  
-    3: {
-      title: `Kitty Town Coffee- Wolfgang's Vanilla Buttercream`,
-      price: 10.99,
-      id: 1,
-      quantity: 1,
-      image: wolfgangTurned,
-      category: 'light roast',
-      desc: `Elevate your coffee experience to the extraordinary with Wolfgang's Vanilla Buttercream, a magical creation by Kitty Town Coffee. Inspired by the culinary artistry of Wolfgang himself, this blend combines the velvety smoothness of vanilla buttercream with the rich allure of premium coffee beans, resulting in a symphony of flavors that's nothing short of delightful.`
-    },  
-    4: {
-      title: 'Kitty Town Coffee- Fezzik Brazil Caramel',
-      price: 10.99,
-      id: 1,
-      quantity: 1,
-      image: fezzikTurned,
-      category: 'medium roast',
-      desc: `Unveil the enchanting allure of Brazil's finest coffee with Fezzik Brazil Caramel, a remarkable creation from Kitty Town Coffee. Named after the gentle giant Fezzik, this coffee blend offers a mesmerizing journey into the heart of Brazil's coffee tradition, where the sweetness of caramel meets the exotic notes of South American coffee, resulting in a cup of pure delight.`
-    },  
-    5: {
-      title: 'Kitty Town Coffee- Rafiki of Guatemala',
-      price: 10.99,
-      id: 1,
-      quantity: 1,
-      image: rafikiTurned,
-      category: 'medium roast',
-      desc: `Embark on a journey to the heart of Central America with Rafiki of Guatemala, a coffee blend as wise and welcoming as the great Rafiki himself. This coffee pays homage to the rich coffee traditions of Guatemala, where high-altitude coffee farms produce beans celebrated for their incredible flavor, depth, and wisdom.`
-    },  
-    6: {
-        title: `Kitty Town Coffee- Tara's Tiramisu`,
-        price: 10.99,
-        id: 1,
-        quantity: 1,
-        category: 'medium roast',
-        image: taraTurned,
-        desc: `Discover the luscious flavors of Italy's most beloved dessert with Tara's Tiramisu, a coffee blend that brings the enchantment of this classic treat to your coffee cup. Named after the charming Tara, this blend is a symphony of velvety mascarpone, rich cocoa, and smooth coffee, creating a moment of pure indulgence with every sip.`
-    },  
-    7: {
-        title: 'Kitty Town Coffee- The Albert Blend',
-        price: 10.99,
-        id: 1,
-        quantity: 1,
-        category: 'light roast',
-        image: albertTurned,
-        desc: `Get ready to experience the essence of timeless classic coffee with The Albert Blend, a coffee creation that embodies the essence of simplicity and elegance. Named in honor of the incomparable Albert, this blend is crafted to capture the purity and richness of a traditional coffee experience.`
-    },  
-    8: {
-        title: `Kitty Town Coffee- Mika's Butter Nut Toffee`,
-        price: 10.99,
-        id: 1,
-        quantity: 1,
-        category: 'medium roast',
-        image: mikaTurned,
-        desc: `Indulge in a symphony of sweet delight with Mika's Butter Nut Toffee, an extraordinary coffee blend by Kitty Town Coffee. Named after the endearing Mika, this blend marries the luscious flavors of buttery toffee and nutty notes, creating a captivating coffee experience that's rich, sweet, and utterly satisfying.`
-    },  
+const coffeeItems = [
+  {
+    title: `Kitty Town Coffee- Robust Roy Chocolate`,
+    price: 10.99,
+    id: 1,
+    quantity: 1,
+    image: robustRoy,
+    category: 'medium roast',
+    desc: `Indulge your senses in a delightful feline adventure with our newest coffee creation - Robust Roy Chocolate. Crafted with the utmost care and dedication, this extraordinary coffee blend combines the bold strength of a robust roast with the sweet and velvety notes of decadent chocolate. It's the ideal choice for those who crave a luxurious coffee experience that's as bold as a lion yet as smooth as a cat's stride.`
+  },
+  {
+    title: `Kitty Town Coffee- Simba's Sumatra`,
+    price: 10.99,
+    id: 1,
+    quantity: 1,
+    image: simbaTurned,
+    category: 'medium roast',
+    desc: `Prepare to embark on a majestic coffee adventure with Simba's Sumatra, a truly regal offering from Kitty Town Coffee. Just like the great lion king Simba himself, this coffee blend reigns supreme, boasting bold character, deep flavor, and a rich heritage that transports you to the exotic landscapes of Sumatra with every sip.`
+  },  
+  {
+    title: `Kitty Town Coffee- Delilah's Decaf Coffee`,
+    price: 10.99,
+    id: 1,
+    quantity: 1,
+    image: delilahTurned,
+    category: 'decaf',
+    desc: `Experience the serenity of a calm, caffeine-free cup of coffee with Delilah's Decaf, the latest enchanting offering from Kitty Town Coffee. This soothing decaffeinated blend allows you to unwind and indulge in the rich, comforting flavors of coffee without the buzz, making it perfect for anytime relaxation.`
+  },  
+  {
+    title: `Kitty Town Coffee- Wolfgang's Vanilla Buttercream`,
+    price: 10.99,
+    id: 1,
+    quantity: 1,
+    image: wolfgangTurned,
+    category: 'light roast',
+    desc: `Elevate your coffee experience to the extraordinary with Wolfgang's Vanilla Buttercream, a magical creation by Kitty Town Coffee. Inspired by the culinary artistry of Wolfgang himself, this blend combines the velvety smoothness of vanilla buttercream with the rich allure of premium coffee beans, resulting in a symphony of flavors that's nothing short of delightful.`
+  },  
+  {
+    title: 'Kitty Town Coffee- Fezzik Brazil Caramel',
+    price: 10.99,
+    id: 1,
+    quantity: 1,
+    image: fezzikTurned,
+    category: 'medium roast',
+    desc: `Unveil the enchanting allure of Brazil's finest coffee with Fezzik Brazil Caramel, a remarkable creation from Kitty Town Coffee. Named after the gentle giant Fezzik, this coffee blend offers a mesmerizing journey into the heart of Brazil's coffee tradition, where the sweetness of caramel meets the exotic notes of South American coffee, resulting in a cup of pure delight.`
+  },  
+  {
+    title: 'Kitty Town Coffee- Rafiki of Guatemala',
+    price: 10.99,
+    id: 1,
+    quantity: 1,
+    image: rafikiTurned,
+    category: 'medium roast',
+    desc: `Embark on a journey to the heart of Central America with Rafiki of Guatemala, a coffee blend as wise and welcoming as the great Rafiki himself. This coffee pays homage to the rich coffee traditions of Guatemala, where high-altitude coffee farms produce beans celebrated for their incredible flavor, depth, and wisdom.`
+  },  
+  {
+    title: `Kitty Town Coffee- Tara's Tiramisu`,
+    price: 10.99,
+    id: 1,
+    quantity: 1,
+    category: 'medium roast',
+    image: taraTurned,
+    desc: `Discover the luscious flavors of Italy's most beloved dessert with Tara's Tiramisu, a coffee blend that brings the enchantment of this classic treat to your coffee cup. Named after the charming Tara, this blend is a symphony of velvety mascarpone, rich cocoa, and smooth coffee, creating a moment of pure indulgence with every sip.`
+  },  
+  {
+    title: 'Kitty Town Coffee- The Albert Blend',
+    price: 10.99,
+    id: 1,
+    quantity: 1,
+    category: 'light roast',
+    image: albertTurned,
+    desc: `Get ready to experience the essence of timeless classic coffee with The Albert Blend, a coffee creation that embodies the essence of simplicity and elegance. Named in honor of the incomparable Albert, this blend is crafted to capture the purity and richness of a traditional coffee experience.`
+  },  
+  {
+    title: `Kitty Town Coffee- Mika's Butter Nut Toffee`,
+    price: 10.99,
+    id: 1,
+    quantity: 1,
+    category: 'medium roast',
+    image: mikaTurned,
+    desc: `Indulge in a symphony of sweet delight with Mika's Butter Nut Toffee, an extraordinary coffee blend by Kitty Town Coffee. Named after the endearing Mika, this blend marries the luscious flavors of buttery toffee and nutty notes, creating a captivating coffee experience that's rich, sweet, and utterly satisfying.`
   }
-  export default coffeeItems
+];
+
+export default coffeeItems;
