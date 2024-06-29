909f1afc6f4ed61a9a18396d29979a76121a7494
Jordan Ayotte <jordanayotte13@outlook.com>
2024-06-29 14:22:24 -0400

    changed menus to cubic bez in animation timing func

--- src/App.js
+++ src/App.js
@@ -85,18 +85,6 @@ function App() {
 
         <strong className="absolute">{cartLength}</strong>
         </button>
-        {/* <button onClick={() => {setShoppingCartActive(false); setMobileMenuActive(!mobileMenuActive); setMobileMenuShowing(true)}}>
-        <div className="hb w-8 h-8 md:w-16 md:h-16 md:hidden">
-          {mobileMenuShowing ? 
-          <><div style={ mobileMenuActive ? {animation: 'hb1 0.3s linear forwards'} : {animation: 'hb1r 0.3s linear forwards'}} className="hb1"></div>
-          <div style={ mobileMenuActive ? {animation: 'hb2 0.15s linear forwards'} : {animation: 'hb2r 0.15s linear forwards'}} className="hb2"></div>
-          <div style={ mobileMenuActive ? {animation: 'hb3 0.3s linear forwards'} : {animation: 'hb3r 0.3s linear forwards'}} className="hb3"></div></>
-          : <><div className="hb1"></div>
-          <div className="hb2"></div>
-          <div className="hb3"></div></>}
-          
-        </div>
-        </button> */}
         <button
 					className={`hamburger hamburger--collapse ${mobileMenuActive ? 'is-active' : ''}`}
 					onClick={() => {setShoppingCartActive(false); setMobileMenuActive(!mobileMenuActive); setMobileMenuShowing(true)}}
--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function MobileMenu({mobileMenuActive, setMobileMenuActive, slide}) {
   return (
     <div className={mobileMenuActive ? 'requires-no-scroll' : null}>
-    <div style={slide === 'left' ? {animation: 'slideLeft 0.5s ease forwards'} : {animation: 'slideRight 0.5s ease forwards'}} className='w-full text-center p-2 md:w-2/5 z-10 fixed right-0 top-0 h-full bg-neutral-900 text-neutral-300'>
+    <div style={slide === 'left' ? {animation: 'slideLeft 0.3s cubic-bezier(.38,.59,.78,.96) forwards'} : {animation: 'slideRight 0.3s cubic-bezier(.38,.59,.78,.96) forwards'}} className='w-full text-center p-2 md:w-2/5 z-10 fixed right-0 top-0 h-full bg-neutral-900 text-neutral-300'>
         <div className='h-16 md:h-32'></div>
         <div className='w-full h-[80%] flex flex-col gap-10 items-center justify-center font-extrabold text-4xl'>
           <Link onClick={() => setMobileMenuActive(false)} to='/'>Home</Link>
